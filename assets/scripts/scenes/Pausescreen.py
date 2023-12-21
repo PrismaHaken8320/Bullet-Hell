aeb8ab326b5b5c3e37862924c101174b2a181f71
PrismaHaken8320 <emilian.pastella@gmail.com>
2023-12-21 09:05:46 +0100

    Replacing Pausescreen buttons
    Setting the pause screen buttons into the middle.

--- assets/scripts/scenes/Pausescreen.py
+++ assets/scripts/scenes/Pausescreen.py
@@ -20,12 +20,12 @@ class PauseScene(Scene):
         self.background = pygame.sprite.Sprite()
         self.background.rect = (0, 0, WIDTH, HEIGHT)
         self.background.image = pygame.image.load(
-            path_join("assets", "sprites", "backgrounds", "title_screen_wallpaper.jpg")
+            path_join("assets", "sprites", "backgrounds", "background.png")
         ).convert_alpha()
         self.font = pygame.font.Font(path_join("assets", "fonts", "DFPPOPCorn-W12.ttf"), 45)
 
         self.matrix = [[["Continue", self.switch_to_game]], [["Retry", self.retry_game]], [["Back to Title", self.switch_to_titlescene]]]
-        self.ButtonMatrix = SelectButtonMatrix(Vector2(500, 300), self.matrix, self.font, (100, 100, 100), (255, 50, 40))
+        self.ButtonMatrix = SelectButtonMatrix(Vector2(WIDTH/2 - 150, HEIGHT/2 -150), self.matrix, self.font, (100, 100, 100), (255, 50, 40))
     
 
     def process_input(self, events) -> None:
